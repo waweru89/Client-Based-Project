0f6fa164438f6fa2107d526c1b013824ceebf482
MBURU <wawerumburu157@gmail.com>
2024-11-06 03:12:47 +0300

    client website

---
+++ loadHeaderFooter.js
@@ -0,0 +1,15 @@
+function loadHeaderAndFooter() {
+    fetch('header.html')
+        .then(response => response.text())
+        .then(data => {
+            document.getElementById('header').innerHTML = data;
+        });
+
+    fetch('footer.html')
+        .then(response => response.text())
+        .then(data => {
+            document.getElementById('footer').innerHTML = data;
+        });
+}
+
+document.addEventListener('DOMContentLoaded', loadHeaderAndFooter);
